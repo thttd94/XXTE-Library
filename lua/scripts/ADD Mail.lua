dccbfeaa65d24d99b4e6a2aca1560b2e977c8088
thttd94 <114326448+thttd94@users.noreply.github.com>
2026-07-04 21:11:32 +0700

    tyu

---
+++ lua/scripts/ADD Mail.lua
@@ -0,0 +1,4 @@
+app.open_url("snssdk473824://webview?url=https://inapp.tiktokv.com/falcon/incentive_campaign/wallet.html")
+sys.msleep(1200)
+touch.tap(520, 760)
+
---
+++ lua/scripts/Link Rut.lua
@@ -0,0 +1,6 @@
+app = require("app")
+sys = require("sys")
+
+app.open_url("snssdk473824://webview?url=https%3A%2F%2Finapp.tiktokv.com%2Ffalcon%2Fincentive_campaign%2Fwithdraw.html%3F__status_bar%3Dtrue%26_pia_%3D1%26activity_name%3Dcoin%26aid%3D473824%26campaign%3DJP-Coin%26campaign%3DJP-Pro%26hide_nav_bar%3D1%26should_full_screen%3D1")
+sys.msleep(5000)
+
